de53426b0786c1fcb47db916337ea55a7ae6c615
hr2204 <hr2204@hotmail.com>
2017-04-21 11:35:45 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Redux Example
+
+
+### Getting Started
+
+> npm install
+> npm start
+```
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function (){
+    return [
+        {title: 'javascript: the good parts', pages: 101},
+        {title: 'Harry Potter', pages: 39},
+        {title: 'The dark Tower', pages: 85},
+        {title: 'Eloquent Ruby', pages: 1}
+    ]
+}
